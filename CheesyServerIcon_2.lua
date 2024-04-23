f9b9b13e144ad91df9afab16e80a9a29444a524e
Fertt0 <126420070+Fertt0@users.noreply.github.com>
2024-04-24 00:11:56 +0300

    Update CheesyServerIcon_2.lua

--- CheesyServerIcon_2.lua
+++ CheesyServerIcon_2.lua
@@ -36,7 +36,7 @@ local function loadImageMeta()
         ['sizeY'] = ui.imageSize(image_0_source).y,
         ['paddingX'] = 0,
         ['paddingY'] = 0,
-        ['scale'] = 0.1}
+        ['scale'] = 0.7}
 
 
         image_1 = {
