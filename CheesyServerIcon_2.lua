9d54732b75b3059c7c26acd106dcdb56433c4455
Fertt0 <126420070+Fertt0@users.noreply.github.com>
2024-04-25 00:53:29 +0300

    Update CheesyServerIcon_2.lua

--- CheesyServerIcon_2.lua
+++ CheesyServerIcon_2.lua
@@ -22,7 +22,7 @@ ui.setAsynchronousImagesLoading(true)
 --Image Config table that is used by the script. 
 --Duplicate the entire table and increment the numbers by 1.
 --Follow the example from image_0 and image_1 to get an understanding of how it works.
-local image_0_source = 'https://raw.githubusercontent.com/Fertt0/Server-Fertt0/1398bf473070bf7361e53d417718623d8d3eac20/servers%20icon.png'
+local image_0_source = 'https://raw.githubusercontent.com/Fertt0/Server-Fertt0/d97bc1c26752504a64f9b3846fb30b08523e0dbd/servers%20icon1.png'
 local image_1_source = ''
 
 -------------------------------------------------------------------
