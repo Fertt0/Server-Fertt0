6f0e5b5dbecb625d28889715ad0d7ea8b53d1b62
Fertt0 <126420070+Fertt0@users.noreply.github.com>
2024-05-07 00:38:45 +0300

    Update Overtake.lua

--- Overtake.lua
+++ Overtake.lua
@@ -11,7 +11,7 @@ end
 local timePassed = 0
 local totalScore = 0
 local comboMeter = 1
-local comboColor = 1
+local comboColor = 0
 local highestScore = 1
 local dangerouslySlowTimer = 0
 local carsState = {}
